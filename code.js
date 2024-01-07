8959f0ab63f2a9d4aa2af0c48a1b67a2bebf2ebb
Jeroen Van Braeckel <jeroen.vanbraeckel@vandewiele.com>
2024-01-07 12:49:11 +0100

    with code getter

--- code.js
+++ code.js
@@ -1,5 +1,23 @@
-function copyToClipboard() {
-    var codeInput = document.getElementById("access-code");
-    codeInput.select();
-    document.execCommand("copy");
-  }
+
+    // Function to extract code parameter from URL
+    function getCodeFromURL() {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get('code') || '';
+      }
+  
+      // Function to set code value in the text field
+      function setCodeValue() {
+        const codeInput = document.getElementById("access-code");
+        const codeValue = getCodeFromURL();
+        codeInput.value = codeValue;
+      }
+  
+      // Function to copy code to clipboard
+      function copyToClipboard() {
+        const codeInput = document.getElementById("access-code");
+        codeInput.select();
+        document.execCommand("copy");
+      }
+  
+      // Call setCodeValue when the page loads
+      window.onload = setCodeValue;
